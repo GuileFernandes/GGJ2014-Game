ef254238afa23b65fbfdb7afd7c7920ef62a4af0
Guilherme Fernandes Vidal <guileads@gmail.com>
2014-01-25 17:09:22 -0300

    controller animacao testes

--- Assets/Scripts/playerScript.cs
+++ Assets/Scripts/playerScript.cs
@@ -10,10 +10,16 @@ public class playerScript : MonoBehaviour {
 	public bool podePulo;
 	private Vector2 movement;
 
+	private bool facingRight = true;
+
+	private Animator anim;
+
 	GroundDetector groundDetector_script;
 	public GameObject groundDetector;
 
 	void Start () {
+
+		anim = GetComponent<Animator>();
 
 		groundDetector_script = groundDetector.GetComponent<GroundDetector> ();
 
@@ -27,16 +33,24 @@ public class playerScript : MonoBehaviour {
 //		float inputY = Input.GetAxis("Vertical");
 
 		movement = new Vector2 (speed.x * inputX,0);
+		anim.SetFloat("Velocidade", Mathf.Abs(speed.x * inputX) );
 
-		if (inputX > 0)
-			this.transform.rotation = new Quaternion (0, 0, 0, 0);
+		if (inputX > 0 && !facingRight){
+			Flip();
+		}
+//			this.transform.rotation = new Quaternion (0, 0, 0, 0);
 
-		if (inputX < 0)
-			this.transform.rotation = new Quaternion (0, 180, 0, 0);
+		if (inputX < 0 && facingRight){
+			Flip();
+		}
+//			this.transform.rotation = new Quaternion (0, 180, 0, 0);
 		
 		if (Input.GetKeyDown (KeyCode.Space) && groundDetector_script.isGrounded) {
-						rigidbody2D.AddForce (new Vector2 (0, 600f));
-				}
+			Debug.Log("Pulou");
+			anim.SetBool("noChao", false);
+			anim.SetBool("Pulou", true);
+			rigidbody2D.AddForce (new Vector2 (0, 1000f));
+		}
 
 	}
 
@@ -45,15 +59,26 @@ public class playerScript : MonoBehaviour {
 	}
 	
 	void OnCollisionEnter2D(Collision2D coll) {
+		Debug.Log("Entrou");
 		if (coll.gameObject.tag == "chao") {
+			anim.SetBool("noChao", true);
+			anim.SetBool("Pulou", false);
 			tempoPulo = 0;
 			podePulo = true;
 		}
 	}
 	void OnCollisionExit2D(Collision2D coll) {
+		Debug.Log("Saiu");
 		if (coll.gameObject.tag == "chao") {
 			tempoPulo = tempoLimitePulo;
 			podePulo = false;
 		}
+	}
+
+	void Flip(){
+		facingRight = !facingRight;
+		Vector3  theScale = transform.localScale;
+		theScale.x *= -1;
+		transform.localScale = theScale;
 	}
 }
