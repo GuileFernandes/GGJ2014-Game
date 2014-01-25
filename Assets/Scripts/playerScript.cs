5fa9ddb9755f59ef516fecb27e6dee8a7d9f4fd9
Felipe Silva <phelipe.santos.1@gmail.com>
2014-01-24 22:54:26 -0300

    Criação do Script do jogador

---
+++ Assets/Scripts/playerScript.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playerScript : MonoBehaviour {
+	public Vector2 speed = new Vector2(1,0);
+	private Vector2 movement;
+
+	void Start () {
+	
+	}
+	
+
+	void Update () {
+		float inputX = Input.GetAxis("Horizontal");
+		float inputY = Input.GetAxis("Vertical");
+
+		movement = new Vector2 (speed.x * inputX,0);
+
+		if(Input.GetKey(KeyCode.Space)){
+			transform.position += transform.up * +speed.y * Time.deltaTime;
+		}
+	}
+
+	void FixedUpdate(){
+		rigidbody2D.velocity = movement;
+	}
+}
