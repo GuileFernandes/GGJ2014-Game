143cd08a49d6cab50883f6173f2b82623451c34c
Felipe Silva <phelipe.santos.1@gmail.com>
2014-01-25 21:18:41 -0300

    Scripts do Robson && Edição da primeira cena

---
+++ Assets/Scripts/Enemy1.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Enemy1 : MonoBehaviour {
+
+	private float movementSpeed = 5.0f;
+	private float positionIni;
+	private bool VaiParaDireita;
+
+	// Use this for initialization
+	void Start () {
+		positionIni = transform.position.x;
+		VaiParaDireita = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(VaiParaDireita)
+			direita ();
+		else
+			esquerda();
+	}
+
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "player"){
+
+		}
+	}
+
+	void direita(){
+		this.transform.rotation = new Quaternion (0, 180, 0, 0);
+		transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+		if( ( transform.position.x - positionIni ) >= 10){
+			VaiParaDireita = false;
+		}
+	}
+
+	void esquerda(){
+		this.transform.rotation = new Quaternion (0, 0, 0, 0);
+		transform.position -= Vector3.right * movementSpeed * Time.deltaTime;
+		if( transform.position.x < positionIni )
+			VaiParaDireita = true;
+	}
+}
---
+++ Assets/Scripts/EnemyBig.cs
@@ -0,0 +1,62 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class EnemyBig : MonoBehaviour {
+	
+	private float movementSpeed = 3.0f;
+	private float positionIni;
+	private bool VaiParaDireita;
+
+	private float nextWalk = 5;
+	private float walkRate = 5;
+
+	private int vidas = 20;
+
+	// Use this for initialization
+	void Start () {
+		positionIni = transform.position.x;
+		VaiParaDireita = true;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(Time.time > nextWalk){
+			if( VaiParaDireita ){
+				if ( transform.position.x - positionIni <= 5 ){
+					atirar();
+					direita ();
+				}
+				else{
+					VaiParaDireita = false;
+					nextWalk = Time.time + walkRate;
+				}
+			}
+			else{
+				if( transform.position.x > positionIni )
+					esquerda ();
+				else{
+					VaiParaDireita = true;
+					nextWalk = Time.time + walkRate;
+				}
+			}
+		}
+	}
+	
+	void OnCollisionEnter(Collision coll){
+		if (coll.gameObject.tag == "projetilPlayer"){
+			vidas--;
+		}
+	}
+	
+	void atirar(){
+		//FireBall bala = Instantiate (fireBall, transform.position, transform.rotation);
+	}
+
+	void direita(){
+		transform.position += Vector3.right * movementSpeed * Time.deltaTime;
+	}
+	
+	void esquerda(){
+		transform.position += Vector3.left * movementSpeed * Time.deltaTime;
+	}
+}
