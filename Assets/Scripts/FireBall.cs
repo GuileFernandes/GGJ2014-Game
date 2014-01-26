d36c836561f8896a0391fc393608599666269495
Robson Cavalcante <robsoncasousa@gmail.com>
2014-01-26 10:04:32 -0300

    minhas alteracoes

---
+++ Assets/Scripts/FireBall.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class FireBall : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Destroy(gameObject, 10);
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		transform.position = Vector2.MoveTowards(transform.position, new Vector2(1, 1), 1);
+	}	
+}
